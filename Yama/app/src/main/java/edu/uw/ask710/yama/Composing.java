8fad6055c3c50e8fe2a1b7c49bb88da95da86dd5
Anirudh Subramanyam <ask710@uw.edu>
2017-11-02 00:33:58 -0700

    Add sending messages functionality

--- Yama/app/src/main/java/edu/uw/ask710/yama/Composing.java
+++ Yama/app/src/main/java/edu/uw/ask710/yama/Composing.java
@@ -6,15 +6,19 @@ import android.net.Uri;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Composing extends AppCompatActivity {
 
     public static final int REQUEST_SELECT_PHONE_NUMBER = 1;
     public static final String TAG = "Composing";
+    private String number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +37,30 @@ public class Composing extends AppCompatActivity {
                     selectContact();
                 }
             });
+            Button send = (Button) findViewById(R.id.send);
+            send.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    sendMessage();
+                }
+            });
 
 
+        }
+    }
+
+    public void sendMessage(){
+        if(number != null){
+            try {
+                EditText msg = (EditText) findViewById(R.id.message);
+                String message = msg.getText().toString();
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(number, null, message, null, null);
+                Toast.makeText(Composing.this, "SMS successfully sent!", Toast.LENGTH_LONG).show();
+            }catch (Exception e){
+                Log.v(TAG, e.toString());
+                Toast.makeText(Composing.this, "SMS failed to send", Toast.LENGTH_LONG).show();
+            }
         }
     }
 
@@ -54,7 +80,7 @@ public class Composing extends AppCompatActivity {
             Cursor cursor = getContentResolver().query(contactUri, projection, null, null, null);
             if(cursor != null && cursor.moveToFirst()){
                 int numberIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
-                String number = cursor.getString(numberIndex);
+                number = cursor.getString(numberIndex);
                 TextView text = (TextView)findViewById(R.id.number);
                 text.setText("Chosen number: " + number);
                 Log.v(TAG, "Here it is " + number);
